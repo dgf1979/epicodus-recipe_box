3b9563c99a6a60562bef7b96ec4b588b70b51e33
Frank Johnson & Drew <drew@finstrom.us>
2015-05-13 10:45:37 -0700

    add listing all recipes

--- app.rb
+++ app.rb
@@ -2,9 +2,21 @@ require('bundler/setup')
 Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file}
 
+#TEST
 get('/test') do
   @test_var = 'Sinatra OK'
   @db = "ActiveRecord Using: #{ActiveRecord::Base.connection_config[:database].upcase}"
   erb(:test)
   #redirect to('/')
+end
+
+#INDEX
+get ('/') do
+  erb(:index)
+end
+
+##RECIPE
+#READ (list all)
+get('/recipes') do
+  erb(:recipes)
 end
